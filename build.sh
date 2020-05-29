ea1c8a4d3257f06778f38b5fc06a83edebf9f112
Atsushi KAWASAKI <ak@xlix.org>
2020-05-30 05:45:23 +0900

    Define TAG naming rules.
    Initial TAG naming rule is...
        a24k/rocker:<version>.<arch>.<libraries>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo "docker build for arm-unknown-linux-gnueabihf with alsa library"
-docker build --rm -t rust-cross-alsa:arm-linux-gnueabihf .
+echo "docker build for arm-unknown-linux-gnueabihf (armv6hf) with alsa library"
+docker build --rm -t a24k/rocker:latest.armv6hf.alsa .
