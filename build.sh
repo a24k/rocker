b601b1fdccc6aae6d1c042b15c8f5d0ed4c55b26
Atsushi KAWASAKI <ak@xlix.org>
2020-05-30 05:52:21 +0900

    Change TAG naming rules.
    A new TAG naming rule is...
        a24k/rocker:<version>-<arch>-<libraries>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "docker build for arm-unknown-linux-gnueabihf (armv6hf) with alsa library"
-docker build --rm -t a24k/rocker:latest.armv6hf.alsa .
+docker build --rm -t a24k/rocker:latest-armv6hf-alsa .
