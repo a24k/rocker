dfaba0e5a33a84dd21a30d37c5aabe51f167cac6
Atsushi KAWASAKI <ak@xlix.org>
2020-05-31 08:59:52 +0900

    Move alsa files to alsa directory.
    Move alsa files to alsa directory.

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ docker build --rm -t a24k/rocker:latest-armv6hf ./rust
 
 # rust-alsa
 echo "docker build for arm-unknown-linux-gnueabihf (armv6hf) with alsa library"
-docker build --rm -t a24k/rocker:latest-armv6hf-alsa .
+docker build --rm -t a24k/rocker:latest-armv6hf-alsa ./alsa
 
 # prune images
 docker image prune -f
